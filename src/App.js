bd1e3997e9c1039ad25c6008236a465a0244775c
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-10-25 17:28:18 +0900

    🚧 JSX 사용방법 학습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+
+      <div className="list">
+        <h3>{글제목}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
