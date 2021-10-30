80fcc2feca9fecb145feff74eaf2d8d6354d0210
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-10-30 21:03:45 +0900

    🚧 props를 응용한 상세페이지 제작 패턴 학습

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 function Modal(props) {
   return (
     <div className="modal">
-      <h2>{props.글제목[0]}</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
@@ -22,6 +22,7 @@ function App() {
 
   const [따봉, 따봉변경] = useState(0)
   const [modal, modal변경] = useState(false)
+  const [누른제목, 누른제목변경] = useState(0)
 
   function 제목바꾸기() {
     const newArray = [...글제목]
@@ -37,6 +38,28 @@ function App() {
 
       <button onClick={제목바꾸기}>버튼</button>
 
+      {/* <button
+        onClick={() => {
+          누른제목변경(0)
+        }}
+      >
+        버튼1
+      </button>
+      <button
+        onClick={() => {
+          누른제목변경(1)
+        }}
+      >
+        버튼2
+      </button>
+      <button
+        onClick={() => {
+          누른제목변경(2)
+        }}
+      >
+        버튼3
+      </button> */}
+
       <button
         onClick={() => {
           modal변경(!modal)
@@ -45,12 +68,16 @@ function App() {
         모달 열기 버튼
       </button>
 
-      {modal === true ? <Modal 글제목={글제목} /> : null}
+      {modal === true ? <Modal 글제목={글제목} 누른제목={누른제목} /> : null}
 
       {글제목.map((title, i) => {
         return (
           <div className="list">
-            <h3>
+            <h3
+              onClick={() => {
+                누른제목변경(i)
+              }}
+            >
               {title}
               <span
                 onClick={() => {
